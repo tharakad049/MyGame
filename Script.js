d1d4ef95f7bac787b856ce219b95b627ffdff883
DilanTharaka <tharakad049@gmail.com>
2022-03-30 23:34:19 +0530

    add five cars

--- Script.js
+++ Script.js
@@ -1,3 +1,16 @@
 $(function () {
+    var container = $('#container');
+    var car = $('#car');
 
-})
+    var car_1 = $('#car_1');
+    var car_2 = $('#car_2');
+    var car_3 = $('#car_3');
+    var car_4 = $('#car_4');
+    var car_5 = $('#car_5');
+
+    var line_1 = $('#line_1');
+    var line_2 = $('#line_2');
+    var line_3 = $('#line_3');
+    var line_4 = $('#line_4');
+    var line_5 = $('#line_5');
+});
