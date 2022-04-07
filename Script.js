907754d5f3e9311cfe3e288006ef9b0c804a39a1
DilanTharaka <tharakad049@gmail.com>
2022-04-07 12:35:17 +0530

    add function face code

--- Script.js
+++ Script.js
@@ -97,8 +97,8 @@ $(function () {
 
     function repeat() {
         if (game_over == false){
-            if (car){
-               // stopGame();
+            if (face(car, car1) || face(car, car2) || face(car, car3) || face(car, car4) || face(car, car5)) {
+                stopGame();
             }
             countScore++;
             if (countScore % 50 == 0){
@@ -125,5 +125,30 @@ $(function () {
         cancelAnimationFrame(move_right);
         cancelAnimationFrame(move_up);
         cancelAnimationFrame(move_down);
+    }
+    function face(rectone, recttwo){
+
+        var r1 = $(rectone);
+        var r2 = $(recttwo);
+
+        var r1x = r1.offset().left;
+        var r1w = r1.width();
+        var r1y = r1.offset().top;
+        var r1h = r1.height();
+
+        var r2x = r2.offset().left;
+        var r2w = r2.width();
+        var r2y = r2.offset().top;
+        var r2h = r2.height();
+
+        if( r1y+r1h < r2y ||
+            r1y > r2y+r2h ||
+            r1x > r2x+r2w ||
+            r1x+r1w < r2x ){
+            return false;
+        }else{
+            return true;
+        }
+
     }
 });
